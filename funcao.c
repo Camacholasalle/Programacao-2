e9f49ae7309f54cbac8496b3ce8fb0b6fbdcea7d
LucasdaSilvaCamacho <32284738+LucasdaSilvaCamacho@users.noreply.github.com>
2018-02-20 19:13:39 -0300

    Update funcao.c

--- funcao.c
+++ funcao.c
@@ -14,5 +14,4 @@ int main()
 
    printf("boa noite");
    }
-// Trabalho para entregar até o dia 25/02
-//https://docs.google.com/forms/d/e/1FAIpQLSc1T74RCXdHacdJspJnxpnGzc1uj-0XgnVmhYQBy1Y0eQi5Ew/viewform
+
