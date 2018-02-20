c72d19bf03c58109560dbc9a2c1b78c3cbbe7e3d
LucasdaSilvaCamacho <32284738+LucasdaSilvaCamacho@users.noreply.github.com>
2018-02-20 19:13:09 -0300

    Add files via upload

---
+++ funcao.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+  {
+
+   void imprimaboanoite();
+   imprimaboanoite();
+
+
+   return ;
+   }
+
+   void imprimaboanoite(){
+
+   printf("boa noite");
+   }
+// Trabalho para entregar até o dia 25/02
+//https://docs.google.com/forms/d/e/1FAIpQLSc1T74RCXdHacdJspJnxpnGzc1uj-0XgnVmhYQBy1Y0eQi5Ew/viewform
